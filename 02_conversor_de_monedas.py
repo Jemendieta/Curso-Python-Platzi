cfbb46f80323d0b84473e3548b5c1d3de9dde426
Jemendieta <mendieta.86@gmail.com>
2023-01-20 00:25:15 -0500

    agrego programa conversor de monedas actualizado

--- 02_conversor_de_monedas.py
+++ 02_conversor_de_monedas.py
@@ -61,33 +61,105 @@
 
 #* Realizaremos el mismo ejercicio pero ahora con funciones para modularizar nuestro programa
 
-def conversor(tipo_moneda, valor_dolar):
-  pesos = input("¿Cuántos pesos " + tipo_moneda + " quieres convertir?: ")
-  pesos = float(pesos)
-  dolares = pesos / valor_dolar
-  dolares = round(dolares,2)
-  dolares = str(dolares)
-  print("Tienes $ " + dolares + " dólares")
+# def conversor(tipo_moneda, valor_dolar):
+#   pesos = input("¿Cuántos pesos " + tipo_moneda + " quieres convertir?: ")
+#   pesos = float(pesos)
+#   dolares = pesos / valor_dolar
+#   dolares = round(dolares,2)
+#   dolares = str(dolares)
+#   print("Tienes $ " + dolares + " dólares")
 
-menu = """
-Bienvenido al conversor de momedas de Python 🤑💲🤑💲🤑
+# menu = """
+# Bienvenido al conversor de momedas de Python 🤑💲🤑💲🤑
 
-1 - Pesos Colombianos
-2 - Pesos Mexicanos
-3 - Pesos Argentinos
+# 1 - Pesos Colombianos
+# 2 - Pesos Mexicanos
+# 3 - Pesos Argentinos
 
-Elige una opción: """
+# Elige una opción: """
 
-opcion = int(input(menu))
+# opcion = int(input(menu))
+# if opcion == 1:
+#   conversor("Colombianos", 3875)
+# elif opcion == 2:
+#   conversor("Mexicanos",19.81)
+# elif opcion == 3:
+#   conversor("Argentinos", 65)
+# else:
+#   print("Elige una opción válida")
+
+# numeroA = 3
+# numeroB = 4
+
+# ahora haremos un ejemplo con fstrings
+# import os
+# os.system('cls')
+
+# USD = 1
+# ARS = 290
+# COP = 4600
+# MXN = 20
+# PEP = 3.50
+
+# print("Bienvenido al conversor de monedas")
+# print("Elige una de las siguietes opciones de conversión: ")
+# print("1 - Dolares estadounidenses a pesos argentinos")
+# print("2 - Dolares estadounidenses a pesos colombianos")
+# print("3 - Dolares estadounidenses a pesos mexicanos")
+# print("4 - Dolares estadounidenses a soles peruanos")
+
+# opcion = int(input("¿Cuál es tu opción?"))
+
+# if opcion == 1:
+#   dolares = int(input("¿Cuántos dolares tienes?: "))
+#   moneda = dolares * ARS
+#   print(f"Tienes {moneda} pesos")
+# elif opcion == 2:
+#   dolares = int(input("¿Cuántos dolares tienes?: "))
+#   moneda = dolares * COP
+#   print(f"Tienes {moneda} pesos")
+# elif opcion == 3:
+#   dolares = int(input("¿Cuántos dolares tienes?: "))
+#   moneda = dolares * MXN
+#   print(f"Tienes {moneda} pesos")
+# elif opcion == 4:
+#   dolares = int(input("¿Cuántos dolares tienes?: "))
+#   moneda = dolares * PEP
+#   print(f"Tienes {moneda} soles")
+# else:
+#   print("Elige una opción correcta...🤨")
+
+
+# Conversor de pesos a dolares
+ARS = 290
+COP = 4600
+MXN = 20
+PEP = 3.50
+
+print("Bienvenido al conversor de monedas")
+print("Elige una de las siguietes opciones de conversión: ")
+print("1 - Pesos argentinos a dolares estadounidenses")
+print("2 - pesos colombianos a dolares estadounidenses")
+print("3 - Pesos mexicanos a dolares estadounidenses")
+print("4 - Soles peruanos a dolares estadounidenses")
+
+opcion = int(input("¿Cuál es tu opción? "))
+
 if opcion == 1:
-  conversor("Colombianos", 3875)
+  moneda = int(input("¿Cuántos pesos tienes?: "))
+  dolares = moneda / ARS
+  print(f"Tienes {dolares} dolares")
 elif opcion == 2:
-  conversor("Mexicanos",19.81)
+  moneda = int(input("¿Cuántos pesos tienes?: "))
+  dolares = moneda / COP
+  print(f"Tienes {dolares} dolares")
 elif opcion == 3:
-  conversor("Argentinos", 65)
+  moneda = int(input("¿Cuántos pesos tienes?: "))
+  dolares = moneda / MXN
+  print(f"Tienes {dolares} dolares")
+elif opcion == 4:
+  moneda = int(input("¿Cuántos soles tienes?: "))
+  dolares = moneda / PEP
+  print(f"Tienes {dolares} dolares")
 else:
-  print("Elige una opción válida")
-
-numeroA = 3
-numeroB = 4
-
+  print("Elige una opción correcta...🤨")
